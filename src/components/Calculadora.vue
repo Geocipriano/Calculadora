fd280cef159362537aa22f00a67fce0d9dd57fea
Geovany Cipriano <geocipriano28@outlook.com>
2021-01-27 17:52:14 -0300

    Update Calculadora.vue
    Update no layout.

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,35 +1,42 @@
 <template>
   <div class="calculadora">
-      <div class="display">Teste</div>
-      <div>C</div>
-      <div>+/-</div>
-      <div>%</div>
-      <div>+</div>
-      <div>7</div>
-      <div>8</div>
-      <div>9</div>
-      <div>x</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-      <div>-</div>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>+</div>
-      <div class="zero">0</div>
-      <div>.</div>
-      <div>=</div>
+      <div class="display">{{ current }}</div>
+      <div class="btn">C</div>
+      <div class="btn">+/-</div>
+      <div class="btn">%</div>
+      <div class="btn operadores">/</div>
+      <div class="btn">7</div>
+      <div class="btn">8</div>
+      <div class="btn">9</div>
+      <div class="btn operadores">x</div>
+      <div class="btn">4</div>
+      <div class="btn">5</div>
+      <div class="btn">6</div>
+      <div class="btn operadores">-</div>
+      <div class="btn">1</div>
+      <div class="btn">2</div>
+      <div class="btn">3</div>
+      <div class="btn operadores">+</div>
+      <div class="btn zero">0</div>
+      <div class="btn">.</div>
+      <div class="btn operadores">=</div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Calculadora'
+    name: 'Calculadora',
+    data() {
+        return {
+            current: '0',
+        }
+    }
 }
 </script>
 <style scoped>
 .calculadora {
+    margin: 0 auto;
+    width: 400px;
     font-family: sans-serif;
     font-size: 40px;
     display: grid;
@@ -39,9 +46,18 @@ export default {
 
 .display {
     grid-column: 1/5;
-    background-color: aquamarine;
+    background-color: #333;
+    color: #fff;
 }
 .zero {
     grid-column: 1/3;
+}
+.btn {
+    background-color: #eee;
+    border: 1px solid #999;
+}
+.operadores {
+    background-color: orange;
+    color: #fff;
 }
 </style>
