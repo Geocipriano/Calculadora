02e37a0f18b7fea2b400589c14def703d66bdcc5
Geovany Cipriano <geocipriano28@outlook.com>
2021-01-27 15:59:44 -0300

    Layout da calculadora
    Criado o Layout da calculadora.

---
+++ src/components/Calculadora.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="calculadora">
+      <div class="display">Teste</div>
+      <div>C</div>
+      <div>+/-</div>
+      <div>%</div>
+      <div>+</div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+      <div>x</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>-</div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>+</div>
+      <div class="zero">0</div>
+      <div>.</div>
+      <div>=</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Calculadora'
+}
+</script>
+<style scoped>
+.calculadora {
+    font-family: sans-serif;
+    font-size: 40px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(50px auto);
+}
+
+.display {
+    grid-column: 1/5;
+    background-color: aquamarine;
+}
+.zero {
+    grid-column: 1/3;
+}
+</style>
